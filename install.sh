1d07622218ed0244b083fade6e21e2a1c1600851
Garsell <71719630+Edi-ID@users.noreply.github.com>
2021-10-23 11:54:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ apt install wget
 mkdir ~/storage/shared/Downloader
 mkdir -p ~/.config/youtube-dl
 mkdir ~/bin
-wget http://gist.githubusercontent.com/Edi-ID/9547d1fd983917cc0c5223e1217a8b30/raw/fd50ee9bbc47336dbba7cd3cfd756fb2e0a8d1c4/youtube-dl -O ~/bin/termux-url-opener
+wget https://gist.githubusercontent.com/Edi-ID/9547d1fd983917cc0c5223e1217a8b30/raw/10154510dfc7bdf941fba55dc49b4f60f21ffb2f/youtube-dl -O ~/bin/termux-url-opener
 dos2unix ~/bin/termux-url-opener
 echo
 echo "Install Successfully"
